32d4096e9ca44d914834b2db25021fa797ebbb4a
nigilmori <112611073+nigilmori@users.noreply.github.com>
2022-12-21 18:19:36 +0300

    Всё без сохранения в картинку

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -5,9 +5,7 @@ import org.jetbrains.skija.*
 import org.jetbrains.skiko.SkiaLayer
 import org.jetbrains.skiko.SkiaRenderer
 import org.jetbrains.skiko.SkiaWindow
-import org.jetbrains.skiko.hostOs
 import java.awt.Dimension
-import java.awt.Rectangle
 import java.awt.event.*
 import java.awt.event.MouseWheelListener
 import java.io.File
@@ -24,6 +22,8 @@ fun createWindow(title: String) = runBlocking(Dispatchers.Swing) {
     window.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
     window.title = title
     window.addWindowListener(WindowCloser)
+
+
     window.layer.renderer = Renderer(window.layer)
     window.layer.addMouseListener(MouseListener)
     window.layer.addMouseMotionListener(MouseMotionListener)
@@ -65,7 +65,10 @@ fun saveField(field: Field){
     if(State.saveFile.path != "") {
         State.saveFile.createNewFile()
         val saver = State.saveFile.bufferedWriter()
-        saver.write(listOf(field.size.toString(), field.bornRule.toString(), field.liveMinRule.toString(), field.liveMaxRule.toString(), State.cellSize.toInt().toString()).joinToString(" "))
+        saver.write(listOf(field.size.toString(), field.bornRule.map{if(it) "1" else "0"}.joinToString(" "),
+            field.liveRule.map{if(it) "1" else "0"}.joinToString(" "),
+            State.cellSize.toInt().toString())
+            .joinToString(" "))
         saver.newLine()
         saver.write(field.cells.map { if (it.isAlive) "1" else "0" }.joinToString(" "))
         saver.close()
@@ -82,7 +85,8 @@ class Cell(val x: Int, val y: Int, var isAlive: Boolean, var liveTime: Int){
     }
 }
 class Field(val path: String = "./data/CommonField.txt", var size: Int = 20, var cells: List<Cell> = listOf(),
-            var bornRule: Int = 3, var liveMinRule: Int = 2, var liveMaxRule: Int = 3){
+            var bornRule: List<Boolean> = listOf(false, false, false, false, false, false, false, false, false, false),
+            var liveRule: List<Boolean> = listOf(false, false, false, false, false, false, false, false, false, false)){
     init{
         try {
             var iterator = 0
@@ -90,10 +94,9 @@ class Field(val path: String = "./data/CommonField.txt", var size: Int = 20, var
             val fileReader = file.bufferedReader()
             val settings = fileReader.readLine().split(" ")
             size = settings[0].toInt()
-            bornRule = settings[1].toInt()
-            liveMinRule = settings[2].toInt()
-            liveMaxRule = settings[3].toInt()
-            State.cellSize = settings[4].toFloat()
+            bornRule = settings.slice(1..9).map{"1" == it}
+            liveRule = settings.slice(10..18).map{"1" == it}
+            State.cellSize = settings[19].toFloat()
             cells = fileReader.readLine().split(" ")
                 .map { Cell(iterator++ / size + 1, (iterator - 1) % size + 1, it != "0", it.toInt()) }
                 .slice(0 until size * size)
@@ -104,18 +107,26 @@ class Field(val path: String = "./data/CommonField.txt", var size: Int = 20, var
     }
     fun transform(x: Int, y: Int) { cells[(x / State.cellSize.toInt() - 1) * size + y / State.cellSize.toInt() - 1].choose() }
     fun liveTime(x:Int, y: Int): Int = cells[(x / State.cellSize.toInt() - 1) * size + y / State.cellSize.toInt() - 1].liveTime
-    fun mouseIn(): Boolean = (State.mouseX - State.offsetX > State.cellSize && State.mouseX - State.offsetX < min(1200-State.offsetX.toInt(), (size+1)*State.cellSize.toInt()) && (State.mouseY - State.offsetY > State.cellSize && State.mouseY - State.offsetY < (size+1)*State.cellSize))
+    fun mouseIn(): Boolean = (State.mouseX - State.offsetX > State.cellSize &&
+            State.mouseX - State.offsetX < min(1200-State.offsetX.toInt(), (size+1)*State.cellSize.toInt()) &&
+           (State.mouseY - State.offsetY > State.cellSize &&
+            State.mouseY - State.offsetY < (size+1)*State.cellSize))
     fun aliveNeighbours(cell: Cell): Int{
         val coordinat = (cell.x-1)*size + (cell.y-1)
         val squareSize = size*size
-        return listOf(cells[(coordinat+1 + squareSize)%squareSize], cells[(coordinat-1 + squareSize)%squareSize], cells[(coordinat+1 + size + squareSize)%squareSize],
-            cells[(coordinat-1 + size + squareSize)%squareSize], cells[(coordinat+1 - size + squareSize)%squareSize], cells[(coordinat-1 - size + squareSize)%squareSize],
-            cells[(coordinat + size + squareSize)%squareSize], cells[(coordinat- size + squareSize)%squareSize]).count{it.isAlive}
+        return listOf(cells[(coordinat+1 + squareSize)%squareSize],
+            cells[(coordinat-1 + squareSize)%squareSize],
+            cells[(coordinat+1 + size + squareSize)%squareSize],
+            cells[(coordinat-1 + size + squareSize)%squareSize],
+            cells[(coordinat+1 - size + squareSize)%squareSize],
+            cells[(coordinat-1 - size + squareSize)%squareSize],
+            cells[(coordinat + size + squareSize)%squareSize],
+            cells[(coordinat- size + squareSize)%squareSize]).count{it.isAlive}
     }
     fun nextGeneration(){
         cells = cells.map{
-            if(it.isAlive && (this.aliveNeighbours(it) < liveMinRule || this.aliveNeighbours(it) > liveMaxRule)) Cell(it.x, it.y, false, 0)
-            else if(!it.isAlive && this.aliveNeighbours(it) == bornRule) Cell(it.x, it.y, true, 1)
+            if(it.isAlive && (!liveRule[this.aliveNeighbours(it)])) Cell(it.x, it.y, false, 0)
+            else if(!it.isAlive && bornRule[this.aliveNeighbours(it)]) Cell(it.x, it.y, true, 1)
             else Cell(it.x, it.y, it.isAlive, if(it.isAlive) it.liveTime + 1 else it.liveTime)
         }
     }
@@ -124,7 +135,8 @@ class Field(val path: String = "./data/CommonField.txt", var size: Int = 20, var
         cells = (0 until size*size).map{Cell(it/size+1, it%size+1, false, 0)}
     }
     fun randomField(){
-        cells = cells.map{Cell(it.x, it.y, Random.nextBoolean(), 0)}.map{if(it.isAlive) Cell(it.x, it.y, it.isAlive, 1) else it}
+        cells = cells.map{Cell(it.x, it.y, Random.nextBoolean(), 0)}.map{
+            if(it.isAlive) Cell(it.x, it.y, it.isAlive, 1) else it}
     }
 }
 class Button(val x: Int, val y: Int, val w: Int, val h: Int, var name: String){
@@ -137,16 +149,19 @@ class Renderer(val layer: SkiaLayer): SkiaRenderer {
     val font = Font(typeface, 14f)
     val alivePaint = Paint().apply{color = 0xFFFF4500.toInt()}
     val deadPaint = Paint().apply{color = 0xFF228B22.toInt()}
+
     var buttons = listOf(Button(1250, 10, 200, 50, "Next"),
         Button(1250, 80, 200, 50, "Random"),
         Button(1250, 150, 200, 50, "Start"))
+
     @ExperimentalTime
     override fun onRender(canvas: Canvas, width: Int, height: Int, nanoTime: Long) {
         val contentScale = layer.contentScale
         canvas.scale(contentScale, contentScale)
         if(State.isClicked) {
             if (State.field.mouseIn()) {
-                State.field.transform(State.mouseX - State.offsetX.toInt(), State.mouseY - State.offsetY.toInt())
+                State.field.transform(State.mouseX - State.offsetX.toInt(),
+                    State.mouseY - State.offsetY.toInt())
             }
             if (buttons[0].mouseIn()) {
                 State.field.nextGeneration()
@@ -162,6 +177,7 @@ class Renderer(val layer: SkiaLayer): SkiaRenderer {
             }
             State.isClicked = false
         }
+
         if(State.openFile.path != ""){
             State.field = openField()
             State.openFile = File("")
@@ -181,19 +197,27 @@ class Renderer(val layer: SkiaLayer): SkiaRenderer {
         drawPlayField(canvas, State.field, State.offsetX, State.offsetY, State.cellSize)
         drawButtons(canvas, buttons)
         if(State.field.mouseIn()){
-            drawLiveTime(canvas, State.mouseX, State.mouseY, State.field.liveTime(State.mouseX - State.offsetX.toInt(), State.mouseY - State.offsetY.toInt()))
+            drawLiveTime(canvas, State.mouseX, State.mouseY,
+                State.field.liveTime(State.mouseX - State.offsetX.toInt(), State.mouseY - State.offsetY.toInt()))
         }
         layer.needRedraw()
     }
     private fun drawPlayField(canvas: Canvas, field: Field, offsetX: Float, offsetY: Float, cellSize: Float){
-        canvas.drawRect(Rect.makeXYWH(offsetX + cellSize, offsetY + cellSize, cellSize*(field.size), cellSize*(field.size)), deadPaint)
-        field.cells.forEach{if(it.x + offsetX/cellSize >= 0 && it.x + offsetX/cellSize < 1200/cellSize.toInt() && it.y + offsetY/cellSize < 2000/cellSize.toInt() && it.y + offsetY/cellSize >= 0 && it.isAlive){
-            canvas.drawRect(Rect.makeXYWH(offsetX + cellSize*it.x, offsetY + cellSize*it.y, cellSize, cellSize), alivePaint)}}
+        canvas.drawRect(Rect.makeXYWH(offsetX + cellSize, offsetY + cellSize, cellSize*(field.size),
+            cellSize*(field.size)), deadPaint)
+        field.cells.forEach{
+            if(it.x + offsetX/cellSize >= 0 && it.x + offsetX/cellSize < 1200/cellSize.toInt() &&
+                it.y + offsetY/cellSize < 2000/cellSize.toInt() && it.y + offsetY/cellSize >= 0 && it.isAlive){
+                canvas.drawRect(Rect.makeXYWH(offsetX + cellSize*it.x, offsetY + cellSize*it.y,
+                                cellSize, cellSize), alivePaint)
+            }
+        }
         canvas.drawRect(Rect.makeXYWH(1200f, 0f, 600f, 1500f), Paint().apply {color = 0xFFFFFFFF.toInt()})
     }
     private fun drawButtons(canvas: Canvas, buttons: List<Button>){
         buttons.forEach {
-            canvas.drawRect(Rect.makeXYWH(it.x.toFloat(), it.y.toFloat(), it.w.toFloat(), it.h.toFloat()), Paint().apply { color = 0x55FF4500.toInt() })
+            canvas.drawRect(Rect.makeXYWH(it.x.toFloat(), it.y.toFloat(), it.w.toFloat(),
+                it.h.toFloat()), Paint().apply { color = 0x55FF4500.toInt() })
             canvas.drawString(it.name, it.x.toFloat() + it.w / 2 - 20, it.y.toFloat() + it.h/2 + 5, font, deadPaint)
         }
     }
@@ -263,22 +287,9 @@ object KeyboardListener: KeyListener{
             State.offsetX -= 10
         }
     }
-
-    override fun keyReleased(event: KeyEvent) {
-        if(event.keyCode == KeyEvent.VK_S){
-            State.offsetY -= 10
-        }
-        if(event.keyCode == KeyEvent.VK_W){
-            State.offsetY += 10
-        }
-        if(event.keyCode == KeyEvent.VK_A){
-            State.offsetX += 10
-        }
-        if(event.keyCode == KeyEvent.VK_D){
-            State.offsetX -= 10
-        }
-    }
+    override fun keyReleased(event: KeyEvent) {}
 }
+
 object SaveActionListener : ActionListener {
     override fun actionPerformed(event: ActionEvent?) {
         var fileSaver = JFileChooser()
@@ -299,10 +310,12 @@ object OpenActionListener : ActionListener {
 }
 object SizeActionListener : ActionListener {
     override fun actionPerformed(event: ActionEvent?) {
-        val returnVal = JOptionPane.showInputDialog(null, "Size", "Set field size", JOptionPane.YES_NO_OPTION)
+        val returnVal = JOptionPane.showInputDialog(null, "Size", "Set field size",
+            JOptionPane.YES_NO_OPTION)
         if(returnVal != null){
             if(returnVal.toInt() <= 0)
-                JOptionPane.showMessageDialog(null, "Invalid field size", "Error", JOptionPane.ERROR_MESSAGE)
+                JOptionPane.showMessageDialog(null, "Invalid field size", "Error",
+                    JOptionPane.ERROR_MESSAGE)
             else
                 State.field.emptyField(returnVal.toInt())
         }
@@ -310,31 +323,24 @@ object SizeActionListener : ActionListener {
 }
 object RuleActionListener : ActionListener {
     override fun actionPerformed(event: ActionEvent?) {
-        val bornField = JTextField(5)
-        val liveMinField = JTextField(5)
-        val liveMaxField = JTextField(5)
-        val rulePanel = JPanel()
-        rulePanel.add(JLabel("Born: "))
-        rulePanel.add(bornField)
-        rulePanel.add(JLabel("Live min: "))
-        rulePanel.add(liveMinField)
-        rulePanel.add(JLabel("Live max: "))
-        rulePanel.add(liveMaxField)
-        val result = JOptionPane.showConfirmDialog(
-            null, rulePanel,
-            "Set game rules", JOptionPane.OK_CANCEL_OPTION
+        val bornRules = (0..8).map {JCheckBox(it.toString(), State.field.bornRule[it])}
+        val liveRules = (0..8).map {JCheckBox(it.toString(), State.field.liveRule[it])}
+        val bornRulePanel = JPanel()
+        bornRules.forEach{bornRulePanel.add(it)}
+        val liveRulePanel = JPanel()
+        liveRules.forEach{liveRulePanel.add(it)}
+        val bornResult = JOptionPane.showConfirmDialog(
+            null, bornRulePanel,
+            "Set born rules", JOptionPane.OK_CANCEL_OPTION
         )
-        if(result == JOptionPane.OK_OPTION){
-            if(bornField.text.toInt() > 0 && bornField.text.toInt() <= 8 &&
-                liveMinField.text.toInt() > 0 && liveMinField.text.toInt() <= 8 &&
-                liveMaxField.text.toInt() >= liveMinField.text.toInt() && liveMaxField.text.toInt() <= 8) {
-                State.field.bornRule = bornField.text.toInt()
-                State.field.liveMinRule = liveMinField.text.toInt()
-                State.field.liveMaxRule = liveMaxField.text.toInt()
-            }
-            else{
-                JOptionPane.showMessageDialog(null, "Invalid input", "Error", JOptionPane.ERROR_MESSAGE)
-
+        if(bornResult == JOptionPane.OK_OPTION){
+            val liveResult = JOptionPane.showConfirmDialog(
+                null, liveRulePanel,
+                "Set live rules", JOptionPane.OK_CANCEL_OPTION
+            )
+            if(liveResult == JOptionPane.OK_OPTION){
+                State.field.bornRule = bornRules.map{it.isSelected}
+                State.field.liveRule = liveRules.map{it.isSelected}
             }
         }
     }
@@ -349,7 +355,8 @@ object SetNumberOfMovesListener: ActionListener{
         numberOfMovesPanel.add(isInfinity)
         numberOfMovesPanel.add(JLabel("Moves: "))
         numberOfMovesPanel.add(numberOfMoves)
-        val result = JOptionPane.showConfirmDialog(null, numberOfMovesPanel, "Set number of moves", JOptionPane.OK_CANCEL_OPTION)
+        val result = JOptionPane.showConfirmDialog(null, numberOfMovesPanel, "Set number of moves",
+            JOptionPane.OK_CANCEL_OPTION)
         if(result == JOptionPane.OK_OPTION) {
             if (isInfinity.isSelected)
                 State.numberOfMoves = -1
@@ -360,7 +367,8 @@ object SetNumberOfMovesListener: ActionListener{
                     else
                         "Error".toInt()
                 } catch(e: Exception){
-                    JOptionPane.showMessageDialog(null, "Invalid number of moves", "Error", JOptionPane.ERROR_MESSAGE)
+                    JOptionPane.showMessageDialog(null, "Invalid number of moves", "Error",
+                        JOptionPane.ERROR_MESSAGE)
                 }
             }
         }
@@ -369,7 +377,8 @@ object SetNumberOfMovesListener: ActionListener{
 
 object WindowCloser: WindowAdapter() {
     override fun windowClosing(event: WindowEvent?) {
-        val result = JOptionPane.showConfirmDialog(null, "Save the state of the field?", "Autosave", JOptionPane.YES_NO_OPTION)
+        val result = JOptionPane.showConfirmDialog(null, "Save the state of the field?", "Autosave",
+            JOptionPane.YES_NO_OPTION)
         if(result == 0){
             State.saveFile = File("./data/CommonField.txt")
             saveField(State.field)
